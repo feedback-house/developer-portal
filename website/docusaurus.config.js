c305655843e00796bf145a2eb3452df5ee382177
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2022-04-18 19:18:13 -0300

    Revert "feat(remove-baseurl): removed baseurl for docusaurus deploy (#32)"
    This reverts commit f5697c5fd1096b02f5eace4d94ee7aaa97c54cb4.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports={
   "title": "Feedback House for Developers",
   "tagline": "One stop shop for teams",
   "url": "https://developers.feedback.house",
-  "baseUrl": "/",
+  "baseUrl": "/developer-portal/",
   "organizationName": "feedback-house",
   "projectName": "developer-portal",
   "scripts": [
